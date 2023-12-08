eda4c3a40af8885ce112890c0a76cbe037656db2
hcopley <hgcopley@gmail.com>
2023-12-08 10:26:42 -0500

    added image

--- server.R
+++ server.R
@@ -1,1 +1,63 @@
-server <- function(input, output, session) { }
+dat <- read_csv('heart_failure_clinical_records_dataset.csv')
+
+server <- function(input, output, session) { 
+    
+    
+    partition_data <- function(.data, model) {
+        
+        set.seed(42)
+        partition <- createDataPartition(dat$DEATH_EVENT, p = input$split/100, list = FALSE) 
+        
+        train_set <- dat[partition,] 
+        
+        test_set <- dat[-partition,]
+        
+        variables <- switch(model, 
+               'rf' = input$var_rf,
+               'logit' = input$var_logit
+               )
+        
+        list(train_set, test_set)
+    }
+    
+    fit_dat <- observeEvent(input$fit, {
+        set.seed(42)
+        partition <- createDataPartition(dat$DEATH_EVENT, p = input$split/100, list = FALSE) 
+        
+        train_set <- dat[partition,] 
+        
+        test_set <- dat[-partition,]
+        
+        train_rf <- train_set %>%
+            select(input$var_rf)
+        
+        train_logit <- train_set %>%
+            select(input$var_logit)
+        
+        
+        
+    })
+    
+   
+    #split data into train/test
+    output$split_cols <- renderText({
+        
+        get_dat()
+        
+        paste0("There are ", nrow(train_set)," observations in the training set and ", nrow(test_set), " observations in the test set.")
+        
+    })
+    
+    
+    
+    output$train_rf_cols <- renderText({
+        
+        get_dat()
+        
+        train_set %>%
+            colnames()
+    
+    })
+    
+    
+    }
--- ui.R
+++ ui.R
@@ -1,29 +1,77 @@
 library(shinydashboard)
+library(tidyverse)
+library(caret)
+library(bslib)
 
 dashboardPage(
+    skin = "purple",
     dashboardHeader(),
     dashboardSidebar(
-        
         sidebarMenu(
             menuItem("About", tabName = "about", icon = icon("circle-info")),
             menuItem("Data Exploration", tabName = "data_exploration", icon = icon("compass")),
-            menuItem("Modeling", tabName = "model", icon = icon("brain"),
+            menuItem("Modeling", tabName = "model_info", icon = icon("brain"),
                      startExpanded = FALSE,
                      menuSubItem("Modeling Info",tabName = "model_info"),
                      menuSubItem("Model Fitting",tabName = "model_fit"),
+                     
+                     sliderInput(
+                         inputId = "split",
+                         label = "Training Data Split Percentage",
+                         min = 0,
+                         max = 100,
+                         post = " %",
+                         value = 80,
+                     ),
+                     
+                    selectizeInput(
+                            inputId = "var_logit",
+                            label = "Predictor Variables for Logistic Regression",
+                            choices = colnames(dat),
+                            multiple = TRUE
+                        ),
+                    
+                    selectizeInput(
+                        inputId = "var_rf",
+                        label = "Predictor Variables for Random Forest",
+                        choices = colnames(dat),
+                        multiple = TRUE
+                    ),
+                    
+                    sliderInput(inputId = "mtry_range", 
+                                label ="Range for Tuning mtry in Random Forest", 
+                                min = 1, 
+                                max = 12, 
+                                value = c(1, 4)
+                    ),
+                    
+                    actionButton("fit", "Fit Models"),
+                        
                      menuSubItem("Prediction",tabName = "model_predict")
                      
                      )
         )
     ),
     dashboardBody( dashboardBody(
+        
+        
         tabItems(
-            tabItem(tabName = "about", h2("About")),
+            tabItem(tabName = "about", h2("About"),
+                    
+                    #tags$img(src = "https://upload.wikimedia.org/wikipedia/commons/7/70/Fluent_Emoji_Color_1fac0.svg", height = "200px")
+                    tags$img(src = "https://upload.wikimedia.org/wikipedia/commons/3/32/Noto_Emoji_v2.034_1fac0.svg", height = "200px")
+                    
+                    ),
             tabItem(tabName = "data_exploration", h2("Data Exploration")),
-            tabItem(tabName = "model", h2("Modeling")),
-            tabItem(tabName = "model_info", h2("Modeling")),
-            tabItem(tabName = "model_fit", h2("Modeling")),
-            tabItem(tabName = "model_prediction", h2("Modeling"))
+            tabItem(tabName = "model_info", h2("Model Info")),
+            tabItem(tabName = "model_fit", h2("Model Fitting"),
+                    
+                    textOutput("split_cols"),
+                    textOutput("train_rf_cols")
+                    
+                    ),
+            tabItem(tabName = "model_predict", h2("Model Prediction"))
             )
         ))
 )
+
